da3e8deaee02f8f75d925fc17bc7df51aba7a2ba
JuicetenG <127890628+JuicetenG@users.noreply.github.com>
2023-11-20 16:23:18 -0500

    add operate function with logic for choosing mathematical function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let firstNumber; 
+let secondNumber;
+let operator;
+
 function add(a, b){
     return a + b;
 }
@@ -12,5 +16,23 @@ function multiply(a, b){
 
 function divide(a, b){
     return a / b;
+}
+
+function operate(a, b, operator){
+    switch (operator){
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+    }
+
 }
 
